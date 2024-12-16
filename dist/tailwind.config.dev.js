7fbe1a1c4a3735d0ec671453eceb4d4c2f0e964f
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-16 05:43:23 +0100

    Having fun

--- dist/tailwind.config.dev.js
+++ dist/tailwind.config.dev.js
@@ -15,7 +15,8 @@ module.exports = {
       },
       backgroundImage: {
         gradient: "linear-gradient(90deg, #F8BFBF 0%, #EE8B8B 100%)",
-        gradientLight: "linear-gradient(90deg, #FFFFFF 0%, #FFF4F4 100%)"
+        gradientLight: "linear-gradient(90deg, #FFFFFF 0%, #FFF4F4 100%)",
+        desktop: "url('/images/bg-pattern-desktop.svg')"
       },
       fontFamily: {
         Joseph: ["Josefin Sans", "sans-serif"]
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       backgroundImage: {
         gradient: "linear-gradient(90deg, #F8BFBF 0%, #EE8B8B 100%)",
         gradientLight: "linear-gradient(90deg, #FFFFFF 0%, #FFF4F4 100%)",
+        desktop: "url('/images/bg-pattern-desktop.svg')"
       },
       fontFamily: {
         Joseph: ["Josefin Sans", "sans-serif"]
