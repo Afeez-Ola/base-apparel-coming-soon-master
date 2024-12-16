357459d51efb371e4d7ea701a26209cd6cf7f49e
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-16 03:51:04 +0100

    setup the backgroundcolor in the tailwind config

--- dist/tailwind.config.dev.js
+++ dist/tailwind.config.dev.js
@@ -6,13 +6,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        gradient: "linear-gradient(90deg, #F8BFBF 0%, #EE8B8B 100%)",
         brown: {
           dark: "#423A3A",
           "default": "#CE9898",
           light: "#EE8B8B"
         },
         white: "#FFFFFF"
+      },
+      backgroundColor: {
+        gradient: "linear-gradient(90deg, #F8BFBF 0%, #EE8B8B 100%)"
       }
     }
   },
